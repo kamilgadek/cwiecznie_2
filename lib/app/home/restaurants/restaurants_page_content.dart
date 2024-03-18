d1228cb4a7bb325aa83c7eef2938889074dd3960
Kamil Gadek <kamil.gadek124@gmail.com>
2024-03-18 19:31:43 +0100

    Add restaurants cubit

--- lib/app/home/restaurants/restaurants_page_content.dart
+++ lib/app/home/restaurants/restaurants_page_content.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cwiecznie_2/app/home/restaurants/cubit/restaurants_cubit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ResturantsPageContent extends StatelessWidget {
   const ResturantsPageContent({
@@ -9,50 +9,48 @@ class ResturantsPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream:
-            FirebaseFirestore.instance.collection('restaurants').orderBy('rating', descending: true).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
+    return BlocProvider(
+      create: (context) => RestaurantsCubit()..start(),
+      child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          if (state.errorMessage.isNotEmpty) {
+            return Center(
+              child: Text('Something went wrong: ${state.errorMessage}'),
+            );
           }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Loading'));
-          } 
-          final documents = snapshot.data!.docs;
+          if (state.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final documents = state.documents;
 
           return ListView(
             children: [
               for (final document in documents) ...[
                 Padding(
                   padding: const EdgeInsets.all(10.0),
-                  child: Dismissible(
-                    onDismissed: (_) {
-                      FirebaseFirestore.instance.collection('restaurants').doc(document.id).delete();
-                    },
-                    key: ValueKey(document.id),
-                    child: Container(
-                      color: Colors.purple[300],
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(document['name']),
-                              Text(document['pizza']),
-                            ],
-                          ),
-                          Text(document['rating'].toString())
-                        ],
-                      ),
+                  child: Container(
+                    color: Colors.purple[300],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(document['name']),
+                            Text(document['pizza']),
+                          ],
+                        ),
+                        Text(document['rating'].toString())
+                      ],
                     ),
                   ),
                 ),
               ],
             ],
           );
-        });
+        },
+      ),
+    );
   }
 }
