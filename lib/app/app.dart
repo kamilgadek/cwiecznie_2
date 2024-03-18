6a7f39f7a513be4ecb15e2fd269e35fc0de567bc
Kamil Gadek <kamil.gadek124@gmail.com>
2024-03-18 20:41:15 +0100

    Implement rootcubit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,8 @@
+import 'package:cwiecznie_2/app/cubit/root_cubit.dart';
 import 'package:cwiecznie_2/app/features/home/home_page.dart';
 import 'package:cwiecznie_2/app/features/login/login_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -24,14 +23,18 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
-          if (user == null) {
-            return LoginPage();
-          }
-          return HomePage(user: user);
-        });
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+
+            final user = state.user;
+                if (user == null) {
+                  return LoginPage();
+                }
+                return HomePage(user: user);
+        },
+      ),
+    );
   }
 }
